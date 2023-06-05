800a4445b464454c864160f16939842c6c87afde
NicoleSanchez9 <135658225+NicoleSanchez9@users.noreply.github.com>
2023-06-05 19:38:59 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+
+let storyOpening = document.querySelector(".story-opening");
+let optionOneScreen = document.querySelector(".option-one-screen");
+let buttonOne = document.querySelector(".option-one");
+  
+
+storyOpening.style= "display:block";
+
+
+
+
+
+buttonOne.onclick = function(){
+  let storyOpening = document.querySelector(".story-opening");
+  storyOpening.style.display = "block";
+  storyOpening.style = "display:block";
+  optionOneScreen.style = "display:block";
+};
+
+
+
+
+
